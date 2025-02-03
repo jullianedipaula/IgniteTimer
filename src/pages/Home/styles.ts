e0e682f47ca2ec56658c3b3fd7f87dc8e5e2a306
Julliane Di Paula <dipaulajulliane@gmail.com>
2025-02-03 18:20:04 -0300

    :recycle: Aprimorando os inputs

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -50,11 +50,15 @@ const BaseInput = styled.input`
 
 
 export const TaskInput = styled(BaseInput)`
-  flex: 1;
+    flex: 1;
+
+    &::-webkit-calendar-picker-indicator {
+        display: none !important;
+    }	
 `
 
 export const MinutesAmountInput = styled(BaseInput)`
-width: 4rem;
+    width: 4rem;
 `;
 
 export const CountdownContainer = styled.div`
