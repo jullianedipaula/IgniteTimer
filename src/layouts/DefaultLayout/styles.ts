e14234d088f88b6ead40fa2a1c5e7f9c3f626354
Julliane Di Paula <dipaulajulliane@gmail.com>
2025-02-03 17:04:34 -0300

    :sparkles: feat: Componentização do Header

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -6,4 +6,9 @@ export const LayoutContainer = styled.div`
     margin: 5rem auto;
     padding: 2.5rem;
 
+   background: ${props => props.theme['gray-800']};
+    border-radius: 8px;
+    
+    display: flex;
+    flex-direction: column;
 `;
