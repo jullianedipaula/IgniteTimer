b19d134de3a92b230c181f2a4bbfe66e8906db59
Julliane Di Paula <dipaulajulliane@gmail.com>
2025-02-03 21:53:38 -0300

    :sparkles: feat: Adicionando Funcionalidades aos Forms com React Hook Form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,23 +1,53 @@
 import { Play } from "phosphor-react";
-import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from "./styles";
+import { useForm } from "react-hook-form";
 
 export function Home() {
+  const { register, handleSubmit, watch } = useForm();
+
+  function handleCreateNewCycle(data: any) {
+    console.log(data);
+  }
+
+  const task = watch("task");
+  const isSubmitDisabled = !task;
+
   return (
     <HomeContainer>
-      <form action="">
-
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" list="task-suggestion" placeholder="Dê um nome para o seu projeto"/>
+          <TaskInput
+            id="task"
+            list="task-suggestion"
+            placeholder="Dê um nome para o seu projeto"
+            {...register("task")}
+          />
 
           <datalist id="task-suggestions">
-            <option value="Projeto 1"/>
-            <option value="Projeto 2"/>
-            <option value="Projeto 3"/>
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
           </datalist>
 
           <label htmlFor="">durante</label>
-          <MinutesAmountInput type="number" id="minutesAmount" placeholder="00" step={5} max={60} min={5} />
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            max={60}
+            min={5}
+            {...register("minutesAmount", { valueAsNumber: true })}
+          />
 
           <span>minutos.</span>
         </FormContainer>
@@ -30,9 +60,9 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton type="submit">
-            <Play size={24}/>
-            Começar
+        <StartCountdownButton disabled={!isSubmitDisabled} type="submit">
+          <Play size={24} />
+          Começar
         </StartCountdownButton>
       </form>
     </HomeContainer>
